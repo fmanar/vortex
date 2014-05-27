f511df34db7bbd900152adea47de66e38d1ca900
Field Manar <fmanar@umd.edu>
2014-05-27 16:16:44 -0400

    updated io functions

--- io/ReadVC7File.m
+++ io/ReadVC7File.m
@@ -1,7 +1,17 @@
-function [x y u v] = ReadVC7File( file )
+function [ s ] = ReadVC7File( file )
+% Reads in a VC7 file from DaVis
+% actuall just encapsulates two functions from LaVision
+%
+% Inputs:
+%     - file: the file name
+% Output:
+%     - s: the vector field structure
 
 A = readimx(file);
-[x y u v] = showimx(A,1);
+[s.x s.y s.u s.v] = showimx(A,1);
+[s.Ny s.Nx] = size(s.x);
+s.dx = abs( x(2,1) - x(1,1) );
+s.dy = abs( y(1,2) - y(1,1) );
 
 return
 
--- io/ReadVectorFile.m
+++ io/ReadVectorFile.m
@@ -1,42 +1,53 @@
-% Loads an ASCII file from DaVis
+function [ s ] = ReadVectorFile( file )
+% Reads in a vector file from DaVis
+% for reference, a vector file looks something like this:
+% #DaVis 8.1.2 2D-vector 8 205 324 "" "mm" "" "mm" "velocity" "m/s"
+% -59.3761	51.4637	0	-0
+% -58.97	51.4637	0	-0
+% -58.5639	51.4637	0	-0
+% -58.1578	51.4637	0	-0
+% ...
+%
+% Inputs:
+%     - file: the file name
+% Output:
+%     - s: the vector field structure
 
-function [x, y, u, v, dx, dy] = ReadVectorFile(name)
-
-f = fopen(name);
+f = fopen(file);
 
 % opening line
 data = textscan(f,'%s %s %s %u %u %u %s %s %s %s %s %s',1);
 if ~strcmp(data{1},'#DaVis')
-    fprintf('Error: not a DaVis file.\n%s',name);
-    x = 0;
-    y = 0;
-    u = 0;
-    v = 0;
+    fprintf('Error: not a DaVis file.\n%s',file);
+    s.x = 0;
+    s.y = 0;
+    s.u = 0;
+    s.v = 0;
     return
 end
-ypts = data{5};
-xpts = data{6};
+s.Ny = data{5};
+s.Nx = data{6};
 
 % rest of file
-x = zeros(xpts,ypts);
-y = zeros(xpts,ypts);
-u = zeros(xpts,ypts);
-v = zeros(xpts,ypts);
+s.x = zeros(xpts,Ny);
+s.y = zeros(xpts,Ny);
+s.u = zeros(xpts,Ny);
+s.v = zeros(xpts,Ny);
 
 data = textscan(f,'%f %f %f %f');
 c = 1;
-for j = 1:ypts
-    for i = 1:xpts
-        x(i,j) = data{1}(c);
-        y(i,j) = data{2}(c);
-        u(i,j) = data{3}(c);
-        v(i,j) = data{4}(c);
+for j = 1:s.Ny
+    for i = 1:s.Nx
+        s.x(i,j) = data{1}(c);
+        s.y(i,j) = data{2}(c);
+        s.u(i,j) = data{3}(c);
+        s.v(i,j) = data{4}(c);
         c = c+1;
     end
 end
 
 fclose(f);
 
-dx = abs( x(2,1) - x(1,1) );
-dy = abs( y(1,2) - y(1,1) );
+s.dx = abs( x(2,1) - x(1,1) );
+s.dy = abs( y(1,2) - y(1,1) );
 return
